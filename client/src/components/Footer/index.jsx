4059b997b952c9650728a39df283a552078a10ae
Jose Meza <enriquesais123@gmail.com>
2024-04-16 15:30:16 -0500

    hola

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      foo
+    </div>
+  )
+}
+
+export default index
---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      Desde 
+    </div>
+  )
+}
+
+export default index
